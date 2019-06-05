59840f6f6057d00e7968237405a600390cd0511c
Yuki Takazawa <s.yuki.regista@gmail.com>
2019-06-05 23:41:20 +0900

    だいたいできた

--- pfcjudge.py
+++ pfcjudge.py
@@ -1,30 +1,15 @@
 import pandas as pd
 import numpy as np
 import bisect as bs
-import json
-from pprint import pprint
+import pickle
 
-#load nutient data of syushoku
-syushoku_df = pd.read_csv('dinner.csv').fillna(0)
-syushoku_name = syushoku_df['foodname']
-Xp = syushoku_df
-
-#producing sample data
-a = np.random.randint(400000)
-x0 = Xp[a:a+1] #sample data
-print(syushoku_name[a]) #name of the sample data
-
-def pfc_judge(x0,Xp):
+def pfc_judge(x0,Xp,sex,age,phys_act_level):
     #Xp is the dataframe of all food options with their values of all nutrients
     #x0 is the dataframe of food eaten with their values of all nutrients
     #initialization
     list_pfc = ['タンパク質(g)','脂質(kcal)','炭水化物(g)','エネルギー(kcal)']
     Xp = np.array(Xp.loc[:,list_pfc]*np.array([4,1,4,1])) #converting Xp to Xp[pfc] with the value indicating kcal
-    x0 = np.array(x0.loc[:,list_pfc]).reshape(1,4)*np.array([4,1,4,1])
-
-    #appling energy limit
-    energy = 3000 - x0[0][3] #upper-bound for energy in a day
-    Xp = Xp[np.where(Xp[:,3]<energy)] #eliminate food that exceeds the limit of energy
+    x0 = np.array(x0.loc[list_pfc]).reshape(1,4)*np.array([4,1,4,1])
     N = Xp.shape[0] #number of syushoku options
 
     #initialization
@@ -38,113 +23,44 @@ def pfc_judge(x0,Xp):
     ikinokori = np.where(Y0 * Y1 * Y2)[0] #array indicating where pfc ratio is in the designated range
     return ikinokori
 
-def EAR_UL_judge(sex,age,phys_act_level,x0):
-    #list of nutrients used for judging lower-bound amounts
-    list_kagen =  ['タンパク質(g)','ビタミンA(μg)','ビタミンB1(mg)','ビタミンB2(mg)','ナイアシン(mg)',
-    'ビタミンB6(mg)','ビタミンB12(μg)','葉酸(μg)','ビタミンC(mg)','ナトリウム(mg)','カルシウム(mg)',
-    'マグネシウム(mg)','鉄(mg)','亜鉛(mg)','銅(mg)','ヨウ素(μg)','セレン(μg)','モリブデン(μg)']
-
-    #list of nutrients used for judging upper-bound amounts
-    list_jogen = ['ビタミンA(μg)','ビタミンD(μg)','ビタミンE(mg)','ナイアシン(mg)','ビタミンB6(mg)',
-    '葉酸(μg)','カルシウム(mg)','マグネシウム(mg)','リン(mg)','鉄(mg)','亜鉛(mg)','銅(mg)','マンガン(mg)',
-    'ヨウ素(μg)','セレン(μg)','モリブデン(μg)']
-
-    #list of all nutrients
-    list_nutrient = ['エネルギー(kcal)','タンパク質(g)','脂質(kcal)',
-    '脂肪酸 飽和(g)','n-6系 多価不飽和(g)','n-3系 多価不飽和(g)','炭水化物(g)',
-    '食物繊維 総量(g)','ビタミンA(μg)','ビタミンD(μg)','ビタミンE(mg)','ビタミンK(μg)',
-    'ビタミンB1(mg)','ビタミンB2(mg)','ナイアシン(mg)','ビタミンB6(mg)','ビタミンB12(μg)',
-    '葉酸(μg)','パントテン酸(mg)','ビオチン(μg)','ビタミンC(mg)','ナトリウム(mg)',
-    'カリウム(mg)','カルシウム(mg)','マグネシウム(mg)','リン(mg)','鉄(mg)','亜鉛(mg)',
-    '銅(mg)','マンガン(mg)','ヨウ素(μg)','セレン(μg)','クロム(μg)','モリブデン(μg)']
-
-    #load standard values
-    EAR_df = pd.read_csv('EAR_male.csv').fillna(0)[age:age+1].loc[:,list_kagen] #EAR dataframe
-    EAR_array = np.array(EAR_df)[0]
-    UL_df = pd.read_csv('UL_male.csv').fillna(0)[age:age+1].loc[:,list_jogen]
-    UL_array = np.array(UL_df)[0]
-
-    #load sorted argument array
-    arg_df = pd.read_csv('syushoku_argsort.csv')
-    arg_EAR_df = arg_df.loc[:,list_kagen]
-    arg_UL_df = arg_df.loc[:,list_jogen]
-    arg_df = arg_df.loc[:,list_nutrient]
-    arg_EAR_array = np.array(arg_EAR_df)
-    arg_UL_array = np.array(arg_UL_df)
-    arg_array = np.array(arg_df)
-
-    #load syushoku data (and names)
-    syushoku_df = pd.read_csv('syushoku.csv').fillna(0)
-    syushoku_EAR_df = syushoku_df.loc[:,list_kagen]
-    syushoku_UL_df = syushoku_df.loc[:,list_jogen]
-    syushoku_name_array = syushoku_df['foodname']
-    syushoku_df = syushoku_df.loc[:,list_nutrient]
-    syushoku_array = np.array(syushoku_df)
-    syushoku_EAR_array = np.array(syushoku_EAR_df)
-    syushoku_UL_array = np.array(syushoku_UL_df)
-
-    #select the food that satisfies the EAR
-    food_EAR = np.array(x0.loc[:,list_kagen])[0]
-    food_UL = np.array(x0.loc[:,list_jogen])[0]
-    lack_vector = EAR_array - food_EAR #if lv[i]>0 then lack
-    excess_vector = food_UL - UL_array #if ev[i]>0 then excess
-    mask1 = np.array([])
-    mask2 = np.array([])
-    all = [i for i in range(len(arg_array[:,1]))]
-    '''
-    for i in range(len(lack_vector)): #EAR judge
-        current_lack = lack_vector[i]
-        arg_EAR_i = arg_EAR_array[:,i]
-        syushoku_i = syushoku_EAR_array[:,i]
-        syushoku_i = syushoku_i[arg_EAR_i]
-        if current_lack>0:
-            #print(list_kagen[i],lack_vector[i])
-            a = bs.bisect_left(syushoku_i,current_lack)
-            mask_i = arg_EAR_i[:a]
-            #print(mask_i)
-            mask1 = np.array(list((set(np.append(mask1,mask_i)))))
-        ikinokori = (set(all)-set(mask1))
-    print(ikinokori)
-    '''
-
-    for i in range(len(lack_vector)): #EAR judge
-        current_ex = excess_vector[i]
-        arg_UL_i = arg_UL_array[:,i]
-        l = arg_UL_i.shape[0]
-        syushoku_i = syushoku_UL_array[:,i]
-        syushoku_i = syushoku_i[arg_UL_i]
-        if current_ex>0:
-            return np.array(set([]))
-        else:
-            a = bs.bisect_right(syushoku_i,-current_ex)
-            mask_i = arg_UL_i[a:]
-            mask2 = np.array(list((set(np.append(mask2,mask_i)))))
-        ikinokori = set(all) - set(mask2)
-    return np.array(list(ikinokori))
-
 def close_judge(x0,Xp,ikinokori,sex,age,phys_level):
-    list_nutrient = ['エネルギー(kcal)','タンパク質(g)','脂質(kcal)',
-    '脂肪酸 飽和(g)','n-6系 多価不飽和(g)','n-3系 多価不飽和(g)','炭水化物(g)',
+    list_nutrient = np.array(['エネルギー(kcal)','タンパク質(g)',
+    'n-6系 多価不飽和(g)','n-3系 多価不飽和(g)',
     '食物繊維 総量(g)','ビタミンA(μg)','ビタミンD(μg)','ビタミンE(mg)','ビタミンK(μg)',
     'ビタミンB1(mg)','ビタミンB2(mg)','ナイアシン(mg)','ビタミンB6(mg)','ビタミンB12(μg)',
     '葉酸(μg)','パントテン酸(mg)','ビオチン(μg)','ビタミンC(mg)','ナトリウム(mg)',
     'カリウム(mg)','カルシウム(mg)','マグネシウム(mg)','リン(mg)','鉄(mg)','亜鉛(mg)',
-    '銅(mg)','マンガン(mg)','ヨウ素(μg)','セレン(μg)','クロム(μg)','モリブデン(μg)']
-    with open('std_vects.txt') as f:
-        std_vectors = json.load(f)
-    std_vector = np.array(std_vectors[sex][age][phys_level])
-    x0 = np.array(x0)[0]
-    Xp = Xp + x0
-    Xp = Xp.loc[:,list_nutrient]
-    Xp_std = Xp/std_vector
-    lack = np.array([1 for i in range(std_vector.shape[0])]) - Xp_std
-    lack = lack.where(lack>0,0)
-    argmin = lack.std(axis=1).idxmin()
+    '銅(mg)','マンガン(mg)','ヨウ素(μg)','セレン(μg)','クロム(μg)','モリブデン(μg)'])
+    dict = {'male':0,'female':3}
+    en_index = dict[sex] + phys_level
+    with open('energy.pickle', mode='rb') as g:
+        energy = [pickle.load(g).iloc[age,en_index]]
+    with open('nutrient_' + sex + '.pickle', mode='rb') as f:
+        std_vect = np.array(energy + list(pickle.load(f).iloc[age,1:]))
+    zero_index = np.where(std_vect == 0)[0]
+    std_vect = np.delete(std_vect,zero_index)
+    list_nutrient = np.delete(list_nutrient,zero_index)
+    Xp_std = ((Xp+x0).loc[:,list_nutrient])/std_vect
+    lack = np.array([1 for i in range(std_vect.shape[0])]) - Xp_std
+    lack = lack.where(lack>0,lack/2)
+    argmin = np.square(lack).sum(axis=1).idxmin()
     return argmin
 
-ikinokori = pfc_judge(x0,Xp)
-print(ikinokori)
-print(syushoku_name[close_judge(x0,Xp,ikinokori,'male','18-30',1)])
-#print(syushoku_name[pfc_judge(x0,Xp)])
-#print(EAR_UL_judge(sex='male',age=10,phys_act_level=1,x0=x0))
-#print(syushoku_name[EAR_UL_judge(sex='male',age=10,phys_act_level=1,x0=x0)])
+def main(x0,age,sex,phys_act_level):
+    #load nutient data of syushoku
+    #age: index
+    with open('dinner1.pickle',mode='rb') as f:
+        dinner_df = pickle.load(f)
+    dinner_name = dinner_df['foodname']
+    Xp = dinner_df.iloc[:,1:] #dinner dataframe without names of food
+    #producing sample data (will be left out)
+    a = np.random.randint(700000)
+    ikinokori = pfc_judge(x0,Xp,sex,age,phys_act_level)
+    return dinner_name[close_judge(x0,Xp,ikinokori,sex,age,phys_act_level)]
+
+foodlist_df = pd.read_csv('foodlist_to_nutrient2.csv',index_col=0)
+x0 = foodlist_df.loc['pad_thai']*2 #sample data
+print(main(x0,age=8,sex='male',phys_act_level = 2))
+#ageにはage groupのインデックスを入れる（何番目のグループか）
+#sex は　'male' か 'female'のどちらか
+#phys_act_level　は 1,2,3　のどれか
